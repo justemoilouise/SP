c7e642b8be19eba4e24764283562603c81f2ca70
Louise Ann Apostol <louiseann.apostol@gmail.com>
2015-02-08 00:04:28 +0800

    ** implement SVM -- build model -- initialize svm parameters from xml

--- TrainingApp/src/com/training/helpers/FileHelper.java
+++ TrainingApp/src/com/training/helpers/FileHelper.java
@@ -1,5 +1,14 @@
 package com.training.helpers;
 
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import FileHandlers.FileOutput;
 import FileHandlers.FileRead;
 
@@ -10,5 +19,28 @@ public class FileHelper {
 	public FileHelper() {
 		this.output = new FileOutput();
 		this.reader = new FileRead();
+	}
+	
+	public static Hashtable<String, String> readFromXML(String tag) {
+		Hashtable<String, String> results = new Hashtable<String, String>();
+		
+		try{
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			
+			Document doc = db.parse("web.xml");
+			NodeList nodes = doc.getElementsByTagName(tag);
+			
+			if(nodes.getLength() > 0) {
+				NodeList list = nodes.item(0).getChildNodes();
+				for(int i=0; i<list.getLength(); i++) {
+					Node n = list.item(i);
+					results.put(n.getNodeName(), n.getNodeValue());
+				}
+			}
+		}
+		catch(Exception x) {}
+		
+		return results;
 	}
 }
--- TrainingApp/src/com/training/svm/SVMProcessor.java
+++ TrainingApp/src/com/training/svm/SVMProcessor.java
@@ -1,7 +1,12 @@
 package com.training.svm;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
+import com.training.helpers.FileHelper;
+
+import libsvm.svm;
+import libsvm.svm_model;
 import libsvm.svm_parameter;
 import libsvm.svm_problem;
 import Data.SVMModel;
@@ -18,7 +23,15 @@ public class SVMProcessor implements ISVM {
 	@Override
 	public void buildModel() {
 		// TODO Auto-generated method stub
+		svm_parameter params = BuildSVMParameters();
+		svm_problem prob = BuildSVMProblem();
 		
+		svm_model svmModel = svm.svm_train(prob, params);		
+		double accuracy = crossValidate(params, prob);
+		
+		//save to SVMModel
+		model.setModel(svmModel);
+		model.setAccuracy(accuracy);
 	}
 
 	@Override
@@ -33,15 +46,27 @@ public class SVMProcessor implements ISVM {
 		return 0;
 	}
 
-	@Override
-	public double getAccuracy() {
-		// TODO Auto-generated method stub
-		return 0;
+	private svm_parameter BuildSVMParameters() {
+		//read parameters from web.xml
+		Hashtable<String, String> params = FileHelper.readFromXML("svm-parameters");
+		
+		svm_parameter svmParams = new svm_parameter();
+		svmParams.eps = Double.parseDouble(params.get("epsilon"));
+		svmParams.kernel_type = Integer.parseInt(params.get("kernel"));
+		svmParams.svm_type = Integer.parseInt(params.get("type"));
+		svmParams.coef0 = Double.parseDouble(params.get("coef"));
+		svmParams.nu = Double.parseDouble(params.get("nu"));
+		svmParams.C = Double.parseDouble(params.get("cost"));
+		svmParams.gamma = Double.parseDouble(params.get("gamma"));
+		svmParams.degree = Integer.parseInt(params.get("degree"));
+		
+		return svmParams;
 	}
-
-	@Override
-	public String getClassName() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private svm_problem BuildSVMProblem() {
+		//read from temporary file - input
+		svm_problem prob = new svm_problem();
+		
+		return prob;
 	}
 }
