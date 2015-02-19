63d5a73929dfe6b481dede861ba4baac6624de29
justemoilouise <louiseann.apostol@gmail.com>
2015-02-19 17:24:28 +0100

    update reading from file for model

--- Utilities/src/FileHandlers/FileInput.java
+++ Utilities/src/FileHandlers/FileInput.java
@@ -2,6 +2,9 @@ package FileHandlers;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -14,6 +17,7 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import CoreHandler.Prompt;
+import Data.ClassifierModel;
 import Data.Input;
 import Data.Species;
 import Interfaces.IFILE_READ;
@@ -23,57 +27,53 @@ public class FileInput implements IFILE_READ {
 	private double[] values;
 
 	@Override
-	public Input parseInput(Row row, int rowNumber) {
+	public File uploadModelFile() {
 		// TODO Auto-generated method stub
-		int cCount = 0;
-		Iterator<Cell> cellIter = row.iterator();
-		if(rowNumber==0) {
-			keys = new String[row.getPhysicalNumberOfCells()];
-			values = new double[row.getPhysicalNumberOfCells()];
-			while(cellIter.hasNext()) {
-				keys[cCount] = cellIter.next().getStringCellValue();
-				cCount++;
-			}
-		}
-		else {
-			while(cellIter.hasNext()) {
-				values[cCount] = cellIter.next().getNumericCellValue();
-				cCount++;
-			}
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Dat files", "dat");
+		JFileChooser fc = new JFileChooser();
+		fc.setFileFilter(filter);
+		int status = fc.showDialog(null, "Choose file");
+		fc.setVisible(true);
 
-			return setInput();
+		try {
+			if (status == JFileChooser.APPROVE_OPTION) {
+				File file = fc.getSelectedFile();
+				return file;
+			}
+			else if (status == JFileChooser.CANCEL_OPTION);
+		}
+		catch(Exception x) {
+			Prompt.PromptError("ERROR_UPLOAD_FILE");
+		}
+
+		return null;
+	}
+	
+	@Override
+	public ClassifierModel readModelFromDATFile(String filename) {
+		try {
+			FileInputStream fileStream = new FileInputStream(filename);
+			ObjectInputStream objectStream = new ObjectInputStream(fileStream);
+			ClassifierModel model = (ClassifierModel)objectStream.readObject();			
+			objectStream.close();
+			
+			return model;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		return null;
 	}
 	
 	@Override
-	public Species parse(Row row, int rowNumber) {
-		// TODO Auto-generated method stub
-		int cCount = 0;
-		Iterator<Cell> cellIter = row.iterator();
-		if(rowNumber==0) {
-			keys = new String[row.getPhysicalNumberOfCells()];
-			values = new double[row.getPhysicalNumberOfCells()];
-			while(cellIter.hasNext()) {
-				keys[cCount] = cellIter.next().getStringCellValue();
-				cCount++;
-			}
-		}
-		else {
-			while(cellIter.hasNext()) {
-				values[cCount] = cellIter.next().getNumericCellValue();
-				cCount++;
-			}
-
-			return setSpecies();
-		}
-		
-		return null;
-	}
-
-	@Override
-	public File uploadFile() {
+	public File uploadExcelFile() {
 		// TODO Auto-generated method stub		
 		FileNameExtensionFilter filter = new FileNameExtensionFilter("Excel files", "xlsx");
 		JFileChooser fc = new JFileChooser();
@@ -95,7 +95,6 @@ public class FileInput implements IFILE_READ {
 		return null;
 	}
 	
-
 	@Override
 	public ArrayList<Input> readInput(File f) {
 		// TODO Auto-generated method stub
@@ -124,7 +123,7 @@ public class FileInput implements IFILE_READ {
 	}
 	
 	@Override
-	public ArrayList<Species> read(File f) {
+	public ArrayList<Species> readSpecies(File f) {
 		// TODO Auto-generated method stub
 		ArrayList<Species> list = new ArrayList<Species>();
 		
@@ -138,7 +137,7 @@ public class FileInput implements IFILE_READ {
 			while(rowIter.hasNext() && rCount<sheet.getPhysicalNumberOfRows()) {
 				Row row = rowIter.next();
 
-				Species s = parse(row, rCount);
+				Species s = parseSpecies(row, rCount);
 				list.add(s);
 				
 				rCount++;
@@ -148,6 +147,56 @@ public class FileInput implements IFILE_READ {
 		}
 		
 		return list;
+	}
+	
+	@Override
+	public Input parseInput(Row row, int rowNumber) {
+		// TODO Auto-generated method stub
+		int cCount = 0;
+		Iterator<Cell> cellIter = row.iterator();
+		if(rowNumber==0) {
+			keys = new String[row.getPhysicalNumberOfCells()];
+			values = new double[row.getPhysicalNumberOfCells()];
+			while(cellIter.hasNext()) {
+				keys[cCount] = cellIter.next().getStringCellValue();
+				cCount++;
+			}
+		}
+		else {
+			while(cellIter.hasNext()) {
+				values[cCount] = cellIter.next().getNumericCellValue();
+				cCount++;
+			}
+
+			return setInput();
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public Species parseSpecies(Row row, int rowNumber) {
+		// TODO Auto-generated method stub
+		int cCount = 0;
+		Iterator<Cell> cellIter = row.iterator();
+		if(rowNumber==0) {
+			keys = new String[row.getPhysicalNumberOfCells()];
+			values = new double[row.getPhysicalNumberOfCells()];
+			while(cellIter.hasNext()) {
+				keys[cCount] = cellIter.next().getStringCellValue();
+				cCount++;
+			}
+		}
+		else {
+			while(cellIter.hasNext()) {
+				values[cCount] = cellIter.next().getNumericCellValue();
+				cCount++;
+			}
+
+			return setSpecies();
+		}
+		
+		return null;
 	}
 	
 	private Species setSpecies() {
@@ -164,4 +213,6 @@ public class FileInput implements IFILE_READ {
 		
 		return i;
 	}
+	
+	
 }
