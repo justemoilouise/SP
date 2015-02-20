da4b7cc228f2faf5de8463fba170c7c3ad231442
Louise Ann Apostol <louiseann.apostol@gmail.com>
2015-02-21 06:43:28 +0800

    update training script

--- TrainingApp/WebContent/script/training.js
+++ TrainingApp/WebContent/script/training.js
@@ -1,10 +1,73 @@
 $(function() {
+	var readFileInput = function() {
+		var filename = $("#input_file").val();
+		
+		$.ajax({
+			url: 'trainingapp/readdataset',
+			method: "GET",
+			success: function(response) { return response; },
+			error: function() { return null; },
+		});
+	};
+	
+	var readSVMParameters = function() {
+		var params = {};
+		params["cost"] = $("#svm_cost").html();
+		params["gamma"] = $("#svm_gamma").html();
+		params["eps"] = $("#svm_eps").html();
+		params["degree"] = $("#svm_degree").html();
+		params["nu"] = $("#svm_nu").html();
+		params["coef"] = $("#svm_coef").html();
+		
+		return params;
+	};
+	
+	var scale = function(dataset) {
+		$.ajax({
+			url: 'trainingapp/preprocess/scale',
+			data: data,
+			success: function(response) { return response; },
+			error: function() { return null; },
+		});
+	};
+	
+	var reduceFeatures = function(dataset) {
+		$.ajax({
+			url: 'trainingapp/preprocess/scale',
+			data: data,
+			success: function(response) { return response; },
+			error: function() { return null; },
+		});
+	};
+	
+	var preprocess = function(dataset) {
+		var preprocessedData = scale(data);
+		
+		if(preprocessedData  != null)
+			preprocessedData = reduceFeatures(preprocessedData);
+		
+		return preprocessedData;
+	};
+	
+	var buildModel = function(dataset) {
+		$.ajax({
+			url: 'trainingapp/svm/buildmodel',
+			data: data,
+			success: function(response) { return response; },
+			error: function() { return null; },
+		});
+	};
+	
 	$("#content_holder").on("click","#train_build_btn", function() {
 		alert("Build model");
 		
 		var dataset = readFile();
 		var preprocessedDataset = preprocess(dataset);
-		buildModel(dataset);
+		var model = buildModel(dataset);
+		
+		if(model != null) {
+			$("#content_holder").load("Training_Output.jsp");
+		}
 	});
 	
 	$("#content_holder").on("click","#train_cancel_btn", function() {
@@ -25,62 +88,3 @@ $(function() {
 		$("#content_holder").load("Training_Input.jsp");
 	});
 });
-
-function readFile() {
-	var filename = $("#input_file").val();
-	
-	$.ajax({
-		url: 'trainingapp/readdataset',
-		method: "GET",
-		success: function(response) { return response; },
-		error: function() { return null; },
-	});
-}
-
-function readSVMParameters() {
-	var params = {};
-	params["cost"] = $("#svm_cost").html();
-	params["gamma"] = $("#svm_gamma").html();
-	params["eps"] = $("#svm_eps").html();
-	params["degree"] = $("#svm_degree").html();
-	params["nu"] = $("#svm_nu").html();
-	params["coef"] = $("#svm_coef").html();
-	
-	return params;
-}
-
-function preprocess(data) {
-	var preprocessedData = scale(data);
-	
-	if(preprocessedData  != null)
-		preprocessedData = reduceFeatures(preprocessedData);
-	
-	return preprocessedData;
-}
-
-function scale(data) {
-	$.ajax({
-		url: 'trainingapp/preprocess/scale',
-		data: data,
-		success: function(response) { return response; },
-		error: function() { return null; },
-	});
-}
-
-function reduceFeatures(data) {
-	$.ajax({
-		url: 'trainingapp/preprocess/scale',
-		data: data,
-		success: function(response) { return response; },
-		error: function() { return null; },
-	});
-}
-
-function buildModel(data) {
-	$.ajax({
-		url: 'trainingapp/svm/buildmodel',
-		data: data,
-		success: function(response) { return response; },
-		error: function() { return null; },
-	});
-}
