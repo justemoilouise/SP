36f508ace3f1042cdcfe09a4adfe8bbc8779d8e0
justemoilouise <louiseann.apostol@gmail.com>
2015-03-04 13:35:42 +0100

    refactor and clean up

--- ClassifierApp/src/gui/OutputPanel.java
+++ ClassifierApp/src/gui/OutputPanel.java
@@ -16,7 +16,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
-import javax.swing.JTextArea;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
