217d00aefa2c6f416553cf412230adcc6208b3c0
Louise Ann Apostol <louiseann.apostol@gmail.com>
2015-05-07 18:00:46 +0800

    ** add GCS jar and dependencies ** remove caching ** revisit model list retrieval

--- TrainingApp/src/com/training/core/TrainingAppServlet.java
+++ TrainingApp/src/com/training/core/TrainingAppServlet.java
@@ -7,8 +7,8 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -29,59 +29,57 @@ import com.google.appengine.api.files.AppEngineFile;
 import com.google.appengine.api.files.FileService;
 import com.google.appengine.api.files.FileServiceFactory;
 import com.google.appengine.api.files.FileWriteChannel;
-import com.google.appengine.api.memcache.MemcacheService;
-import com.google.appengine.api.memcache.MemcacheServiceFactory;
+import com.google.appengine.tools.cloudstorage.GcsFileOptions;
+import com.google.appengine.tools.cloudstorage.GcsFilename;
+import com.google.appengine.tools.cloudstorage.GcsInputChannel;
+import com.google.appengine.tools.cloudstorage.GcsOutputChannel;
+import com.google.appengine.tools.cloudstorage.GcsService;
+import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
+import com.google.appengine.tools.cloudstorage.RetryParams;
 import com.training.data.ClassifierModelList;
-import com.training.helpers.ObjectComparator;
 import com.training.helpers.ServletHelper;
 
 @SuppressWarnings({ "serial", "deprecation" })
 public class TrainingAppServlet extends HttpServlet {
 	private TrainingAppProcessor processor;
 	private BlobstoreService blobstoreService;
-	private MemcacheService memcacheService;
+	private GcsService gcsService;
 	private HttpSession session;
+	final GcsFilename gcsFilename = new GcsFilename("radiss-training.appspot.com", "model-keys");
 	final String cacheModelKey = "model_keys";
 	final String cacheAppKey = "app_keys";
 
 	public TrainingAppServlet() {
 		this.processor = new TrainingAppProcessor();
 		this.blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-		this.memcacheService = MemcacheServiceFactory.getMemcacheService();
+		this.gcsService = GcsServiceFactory.createGcsService(RetryParams.getDefaultInstance());
 	}
-	
-	@SuppressWarnings("unchecked")
+
 	public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		Object response = null;
 		String method = ServletHelper.ExtractMethod(req.getRequestURI());
 		this.session = req.getSession();
 		resp.setContentType("application/json");
-		
+
 		if(method.equalsIgnoreCase("getmodellist")) {
-			ArrayList<ClassifierModelList> models = new ArrayList<ClassifierModelList>();
-			
-			if(memcacheService.contains(cacheModelKey)) {
-				models = (ArrayList<ClassifierModelList>)memcacheService.get(cacheModelKey);
-				Collections.sort(models, new ObjectComparator());
-			}
-			
+			ArrayList<ClassifierModelList> models = getClassifierModels();		
 			response = models;
 		} else if(method.equalsIgnoreCase("getapplist")) {
 			processor.getAppList();
 		}  else if(method.equalsIgnoreCase("upload")) {
 			Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(req);
-	        List<BlobKey> blobKeys = blobs.get("file");
+			List<BlobKey> blobKeys = blobs.get("file");
 
-	        if (blobKeys == null || blobKeys.isEmpty()) {
-	            response = false;
-	        } else {
-	        	session.setAttribute("key", blobKeys.get(0));
-	        	response = true;
-	        }
+			if (blobKeys == null || blobKeys.isEmpty()) {
+				response = false;
+			} else {
+				session.setAttribute("key", blobKeys.get(0));
+				response = true;
+			}
 		} else if(method.equalsIgnoreCase("readdataset")) {
 			InputStream stream = readFileFromBlob((BlobKey) session.getAttribute("key"));
 			ArrayList<Species> dataset = processor.readDataset(stream);
-			
+
 			if(!dataset.isEmpty()) {
 				session.setAttribute("dataset", dataset);
 				response = true;
@@ -93,28 +91,28 @@ public class TrainingAppServlet extends HttpServlet {
 			ClassifierModel model = ServletHelper.ConvertToObject(requestBody, ClassifierModel.class);
 			model = processor.buildClassifierModel(model);
 			BlobKey key = writeFileToBlob(model);
-			saveToCache(key);
-        	response = true;
+			saveModelToGCS(key);
+			response = true;
 		} else if(method.equalsIgnoreCase("download")) {
 			String modelKey = req.getParameter("modelKey");
 			BlobKey modelBlobKey = new BlobKey(modelKey);			
 			BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(modelBlobKey);
 			resp.setHeader("Content-type", "application/octet-stream");
 			resp.setHeader("Content-Disposition", "attachment; filename=\"" + blobInfo.getFilename() +"\"");
-			
+
 			blobstoreService.serve(modelBlobKey, resp);
 			return;
 		}
 
 		resp.getWriter().println(ServletHelper.ConvertToJson(response));
 	}
-	
+
 	private BlobKey writeFileToBlob(ClassifierModel model) {
 		try {
 			ByteArrayOutputStream bStream = new ByteArrayOutputStream();
 			ObjectOutputStream oStream = new ObjectOutputStream(bStream);
 			oStream.writeObject(model);
-			
+
 			FileService fileService = FileServiceFactory.getFileService();
 			AppEngineFile file = fileService.createNewBlobFile("application/octet-stream", "classifier-model-" + model.getVersion() + ".dat");
 			FileWriteChannel writeChannel = fileService.openWriteChannel(file, true);			
@@ -123,45 +121,45 @@ public class TrainingAppServlet extends HttpServlet {
 
 			return fileService.getBlobKey(file);
 		} catch(Exception ex) {}
-		
+
 		return new BlobKey("");
 	}
-	
+
 	private byte[] readFromBlob(BlobKey key) {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-        long start = 0, end = 1024;
-        boolean flag = false;
+		long start = 0, end = 1024;
+		boolean flag = false;
 
-        do {
-            try {
-                byte[] b = blobstoreService.fetchData(key, start, end);
-                out.write(b);
+		do {
+			try {
+				byte[] b = blobstoreService.fetchData(key, start, end);
+				out.write(b);
 
-                if (b.length < 1024)
-                    flag = true;
+				if (b.length < 1024)
+					flag = true;
 
-                start = end + 1;
-                end += 1025;
+				start = end + 1;
+				end += 1025;
 
-            } catch (Exception e) {
-                flag = true;
-            }
+			} catch (Exception e) {
+				flag = true;
+			}
 
-        } while (!flag);
+		} while (!flag);
 
-        return out.toByteArray();
+		return out.toByteArray();
 	}
-	
+
 	private InputStream readFileFromBlob(BlobKey key) {
 		byte[] filebytes = readFromBlob(key);
-        
-        if(filebytes.length > 0)
-        	return new ByteArrayInputStream(filebytes);
-        
-        return null;
+
+		if(filebytes.length > 0)
+			return new ByteArrayInputStream(filebytes);
+
+		return null;
 	}
-	
+
 	private ClassifierModel readModelFromBlob(BlobKey key) {
 		byte[] objBytes = readFromBlob(key);
         
@@ -180,21 +178,50 @@ public class TrainingAppServlet extends HttpServlet {
         
         return null;
 	}
-	
+        
 	@SuppressWarnings("unchecked")
-	private void saveToCache(BlobKey key) {
-		ArrayList<ClassifierModelList> models = new ArrayList<ClassifierModelList>();
-		ClassifierModel model = readModelFromBlob(key);
-		
-		if(model != null) {
-			ClassifierModelList modelList = new ClassifierModelList(key, model);
-			
-			if(memcacheService.contains(cacheModelKey)) {
-				models = (ArrayList<ClassifierModelList>) memcacheService.get(cacheModelKey);
-			}
+	private ArrayList<BlobKey> readModelKeysFromGCS() {
+		ArrayList<BlobKey> modelKeys = new ArrayList<BlobKey>();
 
-			models.add(modelList);
-			memcacheService.put(cacheModelKey, models, null);
+		try {
+			GcsInputChannel readChannel = gcsService.openPrefetchingReadChannel(gcsFilename, 0, 1024 * 1024);
+			ObjectInputStream iStream = new ObjectInputStream(Channels.newInputStream(readChannel));
+			modelKeys = (ArrayList<BlobKey>) iStream.readObject();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
+		return modelKeys;
+	}
+	
+	private void saveModelToGCS(BlobKey key) {
+		ArrayList<BlobKey> modelKeys = readModelKeysFromGCS();
+		modelKeys.add(key);
+
+		try {
+			GcsOutputChannel outputChannel = gcsService.createOrReplace(gcsFilename, GcsFileOptions.getDefaultInstance());
+			ObjectOutputStream oStream = new ObjectOutputStream(Channels.newOutputStream(outputChannel));
+			oStream.writeObject(modelKeys);
+			oStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private ArrayList<ClassifierModelList> getClassifierModels() {
+		ArrayList<ClassifierModelList> models = new ArrayList<ClassifierModelList>();
+		
+		ArrayList<BlobKey> keys = readModelKeysFromGCS();
+		for(BlobKey key : keys) {
+			ClassifierModel model = readModelFromBlob(key);
+			ClassifierModelList modelList = new ClassifierModelList(key, model);
+			models.add(modelList);
+		}
+		return models;
 	}
 }
