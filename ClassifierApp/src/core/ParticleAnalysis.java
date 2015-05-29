d3ed7f373b5f7f991579069ffc7e2c027cada2a9
Louise Ann Apostol <louiseann.apostol@gmail.com>
2015-05-29 12:58:57 +0800

    ** update utilities ** add particle analysis

---
+++ ClassifierApp/src/core/ParticleAnalysis.java
@@ -0,0 +1,60 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import ij.ImagePlus;
+import ij.measure.Measurements;
+import ij.measure.ResultsTable;
+import ij.plugin.filter.ParticleAnalyzer;
+
+public class ParticleAnalysis {
+	private ArrayList<double[]> featureValues;
+	private ArrayList<String> featureLabels;
+	
+	public ParticleAnalysis() {
+		this.featureLabels = new ArrayList<String>();
+		this.featureValues = new ArrayList<double[]>();
+	}
+	
+	public ArrayList<double[]> getFeatureValues() {
+		return featureValues;
+	}
+
+	public ArrayList<String> getFeatureLabels() {
+		return featureLabels;
+	}
+
+	public void analyzeParticles(ImagePlus ip) {
+		ip.getProcessor().findEdges();
+		ResultsTable rt = new ResultsTable();
+		ParticleAnalyzer pa = new ParticleAnalyzer(ParticleAnalyzer.SHOW_NONE, Measurements.SHAPE_DESCRIPTORS, rt, 0, Double.MAX_VALUE, 0, 1);
+		pa.analyze(ip);
+		
+		parseResultTable(rt);
+	}
+	
+	private void parseResultTable(ResultsTable rt) {
+		
+		for(int i=0;; i++) {
+			if(rt.getRowAsString(i) == null) {
+				break;
+			}
+			
+			StringTokenizer value = new StringTokenizer(rt.getRowAsString(i), "\t");
+			double[] v = new double[value.countTokens()];
+			int index = 0;
+			
+			while(value.hasMoreTokens()) {
+				v[index] = Double.parseDouble(value.nextToken());
+				index++;
+			}
+			
+			featureValues.add(v);
+		}
+		
+		for(String val : rt.getHeadings()) {
+			featureLabels.add(val);
+		}
+	}
+}
