9ee2ffbd0250084221e77837f320cd5f9341fdbe
Louise Ann Apostol <louiseann.apostol@gmail.com>
2015-05-31 17:00:20 +0800

    add particle info to species

--- Utilities/src/Data/Species.java
+++ Utilities/src/Data/Species.java
@@ -1,15 +1,19 @@
 package Data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public class Species implements Serializable {
 	private String name;
 	private String[] featureLabels;
 	private double[] featureValues;
+	private String[] particleLabels;
+	private ArrayList<double[]> particleValues;
 	
 	public Species () {
 		this.name = "";
+		this.particleValues = new ArrayList<double[]>();
 	}	
 	public Species(String name, String[] featureLabels, double[] featureValues) {
 		this.name = name;
@@ -33,5 +37,17 @@ public class Species implements Serializable {
 	}
 	public void setFeatureValues(double[] featureValues) {
 		this.featureValues = featureValues;
+	}
+	public String[] getParticleLabels() {
+		return particleLabels;
+	}
+	public void setParticleLabels(String[] particleLabels) {
+		this.particleLabels = particleLabels;
+	}
+	public ArrayList<double[]> getParticleValues() {
+		return particleValues;
+	}
+	public void setParticleValues(ArrayList<double[]> particleValues) {
+		this.particleValues = particleValues;
 	}
 }
