cbe18614bc7a78b09cf5b38da5969437b9469bd6
Louise Ann Apostol <louiseann.apostol@gmail.com>
2015-07-11 19:56:17 +0800

    initial gui components

---
+++ TrainingDesktopApp/src/gui/InitialWindow.java
@@ -0,0 +1,8 @@
+package gui;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class InitialWindow extends JFrame {
+
+}
---
+++ TrainingDesktopApp/src/gui/MainWindow.java
@@ -0,0 +1,8 @@
+package gui;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class MainWindow extends JFrame {
+
+}
