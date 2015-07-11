65ffe033a790f8cbd5da0c04cfcd5f04c379ee5d
Louise Ann Apostol <louiseann.apostol@gmail.com>
2015-07-11 20:18:22 +0800

    initial main window set up

--- TrainingDesktopApp/src/gui/MainWindow.java
+++ TrainingDesktopApp/src/gui/MainWindow.java
@@ -1,8 +1,95 @@
 package gui;
 
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.metal.DefaultMetalTheme;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.OceanTheme;
 
 @SuppressWarnings("serial")
 public class MainWindow extends JFrame {
+	final static String LOOKANDFEEL = "System";
+	final static String THEME = "Test";
+	
+	private boolean mode;
+	
+	public MainWindow(boolean mode) {
+		this.mode = mode;
+		
+		initLookAndFeel();
+		
+		ImageIcon img = new ImageIcon("img/logo.png");
+		
+		setSize(500, 750);
+		setIconImage(img.getImage());
+		setTitle("RadiSS");
+		setBackground(Color.DARK_GRAY);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+	}
+	
+	private void initLookAndFeel() {
+		String lookAndFeel = null;
+
+		if (LOOKANDFEEL != null) {
+			if (LOOKANDFEEL.equals("Metal")) {
+				lookAndFeel = UIManager.getCrossPlatformLookAndFeelClassName();                
+			}
+			else if (LOOKANDFEEL.equals("System")) {
+				lookAndFeel = UIManager.getSystemLookAndFeelClassName();
+			} 
+
+			else if (LOOKANDFEEL.equals("Motif")) {
+				lookAndFeel = "com.sun.java.swing.plaf.motif.MotifLookAndFeel";
+			} 
+
+			else if (LOOKANDFEEL.equals("GTK")) { 
+				lookAndFeel = "com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
+			} 
+
+			else {
+				System.err.println("Unexpected value of LOOKANDFEEL specified: "
+						+ LOOKANDFEEL);
+				lookAndFeel = UIManager.getCrossPlatformLookAndFeelClassName();
+			}
+
+			try {
+				UIManager.setLookAndFeel(lookAndFeel);
+
+				if (LOOKANDFEEL.equals("Metal")) {
+					if (THEME.equals("DefaultMetal"))
+						MetalLookAndFeel.setCurrentTheme(new DefaultMetalTheme());
+					else if (THEME.equals("Ocean"))
+						MetalLookAndFeel.setCurrentTheme(new OceanTheme());
+
+					UIManager.setLookAndFeel(new MetalLookAndFeel()); 
+				}	
+			} 
+
+			catch (ClassNotFoundException e) {
+				System.err.println("Couldn't find class for specified look and feel:"
+						+ lookAndFeel);
+				System.err.println("Did you include the L&F library in the class path?");
+				System.err.println("Using the default look and feel.");
+			} 
+
+			catch (UnsupportedLookAndFeelException e) {
+				System.err.println("Can't use the specified look and feel (" + lookAndFeel
+						+ ") on this platform.");
+				System.err.println("Using the default look and feel.");
+			} 
+
+			catch (Exception e) {
+				System.err.println("Couldn't get specified look and feel (" + lookAndFeel
+						+ "), for some reason.");
+				System.err.println("Using the default look and feel.");
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
