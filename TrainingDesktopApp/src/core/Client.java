0a237f0ffbada5819a3f8c6fde6c7edd5b4cc50f
Louise Ann Apostol <louiseann.apostol@gmail.com>
2015-07-12 15:56:51 +0800

    handle "next"

--- TrainingDesktopApp/src/core/Client.java
+++ TrainingDesktopApp/src/core/Client.java
@@ -13,39 +13,43 @@ public class Client {
 	private static DT decisionTree;
 	private static Preprocess preprocess;
 	private static SVM svm;
-	
+
 	private static ArrayList<Species> dataset;
 	private static int[] mode;
 	private static int currentMode;
-	
+
 	private static InitialWindow initial;
 	private static MainWindow main;
-	
+
 	public Client() {
 		decisionTree = new DT();
 		preprocess = new Preprocess();
 		svm = new SVM();
 		dataset = new ArrayList<Species>();
 		main = new MainWindow();
-		
+
 		initial = new InitialWindow();
 		initial.setVisible(true);
 	}
-	
+
 	public static JCheckBox[] getModes() {
 		return initial.getCheckBoxes();
 	}
-	
+
+	public static int[] getMode() {
+		return mode;
+	}
+
 	public static void setMode(int[] mode) {
 		Client.mode =  mode;
 	}
-	
+
 	public static void showMainWindow() {
 		currentMode = mode[0];
 		main.setVisible(true);
 		showInputPanels();
 	}
-	
+
 	public static void showInputPanels() {
 		if(currentMode == 1) {
 			main.showCard("DT");
@@ -53,28 +57,39 @@ public class Client {
 			main.showCard("SVM");
 		}
 	}
-	
+
+	public static void next() {
+		if(currentMode != mode[mode.length - 1]) {
+			currentMode = mode[1];
+			showInputPanels();
+		}
+		
+		// else, show output
+	}
+
 	public static void setDataset(ArrayList<Species> dataset) {
 		Client.dataset = dataset;
 	}
-	
+
 	public static void trainClassifier() {
 		if(currentMode == 1) {
 			trainDecisionTree();
 		} else if(currentMode == 2) {
 			trainSVM();
 		}
+		
+		next();
 	}
-	
+
 	private static void trainDecisionTree() {
 		decisionTree.setDataSet(dataset);
 		decisionTree.run();
 	}
-	
+
 	private static void trainSVM() {
 		ArrayList<Species> ds = preprocess.scale(dataset);
 		ds = preprocess.reduceFeatures(ds);
-		
+
 		svm.buildModel(ds, null);
 	}
 }
